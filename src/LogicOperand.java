bfffdfe9c3c7f2b44ce3e6c809948b19d166723d
romagolchin <gol4in.roman@yandex.ru>
2017-02-26 20:39:42 +0300

    save

--- src/LogicOperand.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Roman on 20/02/2017.
- */
-public class LogicOperand implements Formula {
-    @Override
-    public Formula substitute(Variable x, Term t) {
-        return null;
-    }
-}
--- src/LogicOperator.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by Roman on 20/02/2017.
- */
-public enum LogicOperator {
-    IMPL, AND, OR, NEG
-}
